baf37aa11a5205e38bac4dea82204399365baa3c
Erich Soares Machado <erichmachado@gmail.com>
2013-07-26 17:35:07 -0300

    Refactored MusicBrainz::BaseModel#validate_type specs to define release_group in general context via let and avoid duplication

--- spec/models/base_model_spec.rb
+++ spec/models/base_model_spec.rb
@@ -6,12 +6,12 @@ describe MusicBrainz::BaseModel do
   describe '#validate_type' do
     describe 'Date' do
       let(:xml) { Nokogiri::XML.parse(response) }
+      let(:release_group) { MusicBrainz::ReleaseGroup.new MusicBrainz::Bindings::ReleaseGroup.parse(xml) }
       
       context 'nil value' do
         let(:response) { '<release-group><first-release-date></first-release-date></release-group>' }
         
         it 'returns 2030-12-31' do
-          release_group = MusicBrainz::ReleaseGroup.new MusicBrainz::Bindings::ReleaseGroup.parse(xml)
           release_group.first_release_date.should == Date.new(2030, 12, 31)
         end
       end
@@ -20,7 +20,6 @@ describe MusicBrainz::BaseModel do
         let(:response) { '<release-group><first-release-date>1995</first-release-date></release-group>' }
         
         it 'returns 1995-12-31' do
-          release_group = MusicBrainz::ReleaseGroup.new MusicBrainz::Bindings::ReleaseGroup.parse(xml)
           release_group.first_release_date.should == Date.new(1995, 12, 31)
         end
       end
@@ -29,7 +28,6 @@ describe MusicBrainz::BaseModel do
         let(:response) { '<release-group><first-release-date>1995-04</first-release-date></release-group>' }
         
         it 'returns 1995-04-30' do
-          release_group = MusicBrainz::ReleaseGroup.new MusicBrainz::Bindings::ReleaseGroup.parse(xml)
           release_group.first_release_date.should == Date.new(1995, 4, 30)
         end
       end
@@ -38,7 +36,6 @@ describe MusicBrainz::BaseModel do
         let(:response) { '<release-group><first-release-date>1995-04-30</first-release-date></release-group>' }
         
         it 'returns 1995-04-30' do
-          release_group = MusicBrainz::ReleaseGroup.new MusicBrainz::Bindings::ReleaseGroup.parse(xml)
           release_group.first_release_date.should == Date.new(1995, 4, 30)
         end
       end
