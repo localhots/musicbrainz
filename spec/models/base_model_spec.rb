cd670e1b0a64334a3edce7fea598d54215e51637
Erich Soares Machado <erichmachado@gmail.com>
2013-07-26 19:12:25 -0300

    Refactored MusicBrainz::BaseModel specs to test only the instance methods (unit) behavior

--- spec/models/base_model_spec.rb
+++ spec/models/base_model_spec.rb
@@ -1,44 +1,50 @@
 # -*- encoding: utf-8 -*-
-
-require "spec_helper"
+require 'spec_helper'
 
 describe MusicBrainz::BaseModel do
-  describe '#validate_type' do
-    describe 'Date' do
-      let(:xml) { Nokogiri::XML.parse(response) }
-      let(:release_group) { MusicBrainz::ReleaseGroup.new MusicBrainz::Bindings::ReleaseGroup.parse(xml) }
+  
+  describe 'InstanceMethods' do
+
+    class MusicBrainz::BaseModelSubclass < MusicBrainz::BaseModel
+    end
+    
+    subject { MusicBrainz::BaseModelSubclass.new }
+    
+    describe '#validate_date' do
+      let(:validated_date) { subject.send(:validate_date, value) }
       
-      context 'when the vale is nil' do
-        let(:response) { '<release-group><first-release-date></first-release-date></release-group>' }
-        
+      context 'when the value is nil' do
+        let(:value) { nil }
+      
         it 'returns a Date object corresponding to 2030-12-31' do
-          release_group.first_release_date.should == Date.new(2030, 12, 31)
+          validated_date.should == Date.new(2030, 12, 31)
         end
       end
-      
+    
       context 'when the value contains only the year' do
-        let(:response) { '<release-group><first-release-date>1995</first-release-date></release-group>' }
-        
+        let(:value) { '1995' }
+      
         it 'returns a Date object corresponding to the last day of the year' do
-          release_group.first_release_date.should == Date.new(1995, 12, 31)
+          validated_date.should == Date.new(1995, 12, 31)
         end
       end
-      
+    
       context 'when the value contains only the year and the month' do
-        let(:response) { '<release-group><first-release-date>1995-04</first-release-date></release-group>' }
-        
+        let(:value) { '1995-04' }
+      
         it 'returns a Date object corresponding to the last day of the month' do
-          release_group.first_release_date.should == Date.new(1995, 4, 30)
+          validated_date.should == Date.new(1995, 4, 30)
         end
       end
-      
+    
       context 'when the value contains the year, month and day' do
-        let(:response) { '<release-group><first-release-date>1995-04-30</first-release-date></release-group>' }
-        
+        let(:value) { '1995-04-30' }
+      
         it 'returns the corresponding Date object' do
-          release_group.first_release_date.should == Date.new(1995, 4, 30)
+          validated_date.should == Date.new(1995, 4, 30)
         end
       end
     end
+
   end
 end
