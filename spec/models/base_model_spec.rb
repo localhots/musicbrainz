3fe5311d5de9c09864e8ee248202b0fc5eda190e
Erich Soares Machado <erichmachado@gmail.com>
2013-07-28 05:35:56 -0300

    Refactored MusicBrainz::BaseModel::InstanceMethods#parse_type specs to remove duplication in specific parsers methods examples

--- spec/models/base_model_spec.rb
+++ spec/models/base_model_spec.rb
@@ -11,47 +11,43 @@ describe MusicBrainz::BaseModel do
     subject { MusicBrainz::BaseModelSubclass.new }
     
     describe '#parse_type' do
-      context 'when the type is Integer' do
-        let(:type) { Integer }
-        let(:value) { '1' }
+      
+      shared_examples 'specific parser' do
         let(:parse_method) { "parse_#{type.name.downcase}".to_sym }
-        
+
         it 'parses the value with the specific implementation' do
           subject.should_receive(parse_method).with(value)
         end
         
         after { subject.send(:parse_type, value, type) }
+      end
+      
+      context 'when the type is Integer' do
+        let(:type) { Integer }
+        let(:value) { '1' }
+        
+        it_should_behave_like 'specific parser'
       end
       
       context 'when the type is Float' do
         let(:type) { Float }
         let(:value) { '0.1' }
-        let(:parse_method) { "parse_#{type.name.downcase}".to_sym }
         
-        it 'parses the value with the specific implementation' do
-          subject.should_receive(parse_method).with(value)
-        end
-        
-        after { subject.send(:parse_type, value, type) }
+        it_should_behave_like 'specific parser'
       end
       
       context 'when the type is Date' do
         let(:type) { Date }
         let(:value) { '2013' }
-        let(:parse_method) { "parse_#{type.name.downcase}".to_sym }
         
-        it 'parses the value with the specific implementation' do
-          subject.should_receive(parse_method).with(value)
-        end
-        
-        after { subject.send(:parse_type, value, type) }
+        it_should_behave_like 'specific parser'
       end
       
-      context 'when there is not a specific implementation for the provided type' do
+      context 'when there is not a specific parser implementation for the provided type' do
         let(:type) { String }
         let(:value) { 'some string' }
         
-        it 'returns the provided value' do
+        it 'just returns the provided value' do
           subject.send(:parse_type, value, type).should eql(value)
         end
       end
