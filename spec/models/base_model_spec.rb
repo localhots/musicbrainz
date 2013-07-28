656feaa2242567e5239761da13e32e15e440be5f
Erich Soares Machado <erichmachado@gmail.com>
2013-07-28 05:30:48 -0300

    Refactored MusicBrainz::BaseModel::InstanceMethods#parse_type specs to not make use of random fixtures for specs

--- spec/models/base_model_spec.rb
+++ spec/models/base_model_spec.rb
@@ -11,9 +11,33 @@ describe MusicBrainz::BaseModel do
     subject { MusicBrainz::BaseModelSubclass.new }
     
     describe '#parse_type' do
-      context 'when there is a specific implementation for the provided type' do
-        let(:type) { [ Integer, Float, Date ].sample }
-        let(:value) { 'sample value' }
+      context 'when the type is Integer' do
+        let(:type) { Integer }
+        let(:value) { '1' }
+        let(:parse_method) { "parse_#{type.name.downcase}".to_sym }
+        
+        it 'parses the value with the specific implementation' do
+          subject.should_receive(parse_method).with(value)
+        end
+        
+        after { subject.send(:parse_type, value, type) }
+      end
+      
+      context 'when the type is Float' do
+        let(:type) { Float }
+        let(:value) { '0.1' }
+        let(:parse_method) { "parse_#{type.name.downcase}".to_sym }
+        
+        it 'parses the value with the specific implementation' do
+          subject.should_receive(parse_method).with(value)
+        end
+        
+        after { subject.send(:parse_type, value, type) }
+      end
+      
+      context 'when the type is Date' do
+        let(:type) { Date }
+        let(:value) { '2013' }
         let(:parse_method) { "parse_#{type.name.downcase}".to_sym }
         
         it 'parses the value with the specific implementation' do
@@ -24,8 +48,8 @@ describe MusicBrainz::BaseModel do
       end
       
       context 'when there is not a specific implementation for the provided type' do
-        let(:type) { [ String, Hash ].sample }
-        let(:value) { type.new }
+        let(:type) { String }
+        let(:value) { 'some string' }
         
         it 'returns the provided value' do
           subject.send(:parse_type, value, type).should eql(value)
