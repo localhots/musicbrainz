78bf676872fa4bbd19f70afb771e386204f0ab44
Emmanuel Turlay <bluesmanu@hotmail.com>
2014-10-02 16:46:45 -0700

    support for artist-credit lookups on recordings

--- lib/musicbrainz/bindings/recording.rb
+++ lib/musicbrainz/bindings/recording.rb
@@ -6,7 +6,8 @@ module MusicBrainz
           position: (xml.xpath('./position').text rescue nil),
           recording_id: (xml.xpath('./recording').attribute('id').value rescue nil),
           title: (xml.xpath('./recording/title').text rescue nil),
-          length: (xml.xpath('./recording/length').text rescue nil)
+          length: (xml.xpath('./recording/length').text rescue nil),
+          artist_credits: (xml.xpath('./recording/artist-credit/name-credit') rescue [])
         }
       end
 
--- lib/musicbrainz/models/recording.rb
+++ lib/musicbrainz/models/recording.rb
@@ -4,10 +4,19 @@ module MusicBrainz
     field :recording_id, String
     field :title, String
     field :length, Integer
+    field :artist_credits, Array
+
+    def full_artist_name
+      @full_artist_name ||= artist_credits.inject('') do |mem, credit|
+        mem << credit.xpath('./artist/name').text rescue ''
+        mem << credit.attributes['joinphrase'].try(:value).to_s
+        mem 
+      end
+    end
 
     class << self
-      def find(id)
-        client.load(:recording, { id: id }, {
+      def find(id, args = {})
+        client.load(:recording, { id: id }.merge(args), {
           binding: :recording,
           create_model: :recording
         })
