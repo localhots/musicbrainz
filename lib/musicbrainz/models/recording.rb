6e0191cdb241bd80577300d387287b6a172052c7
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-01-23 20:36:18 +0100

    Implements limit and offset params for Recording.search. Adds option to search recordings by artist mbid instead of artist name.

--- lib/musicbrainz/models/recording.rb
+++ lib/musicbrainz/models/recording.rb
@@ -8,13 +8,23 @@ module MusicBrainz
     INCLUDES = %w(artists releases artist_credits tags ratings annotation url_rels artist-rels label_rels recording_rels release_rels release_group_rels url_rels work_rels)
     
     class << self
-      def search(artist_name, title, options = {})
-        artist_name = CGI.escape(artist_name).gsub(/\!/, '\!')
-        title = CGI.escape(title).gsub(/\!/, '\!')
-        query = ["artist:\"#{artist_name}\"", "recording:\"#{title}\""]
+      def search(artist, title, options = {})
+        if artist.split('-').length != 5
+          artist = CGI.escape(artist).gsub(/\!/, '\!')  
+        end
+        
+        title = CGI.escape(title).gsub(/\!/, '\!') unless title.nil?
+        
+        query = artist.split('-').length == 5 ? ["arid:\"#{artist}\""] : ["artist:\"#{artist}\""]
+        query << "recording:\"#{title}\"" unless title.nil?
         query << "type: #{options[:type]}" if options[:type]
 
-        client.search(to_s, query.join(' AND '), create_models: false)
+        params = { query: query.join(' AND ') }
+        params[:limit] = options[:limit] if options.has_key? :limit
+        params[:offset] = options[:offset] if options.has_key? :offset
+        options = { create_models: false }.merge(options)
+        
+        client.search(to_s, params, create_models: options[:create_models])
       end
       
       def find_by_artist_and_title(artist_name, title, options = {})
@@ -24,7 +34,7 @@ module MusicBrainz
       
       def by_release_id(release_id)
         client.search(
-          'MusicBrainz::Recording', { query: "reid:#{release_id}", limit: 500 }
+          'MusicBrainz::Recording', { query: "reid:#{release_id}", limit: 100 }
         )
       end
     end
