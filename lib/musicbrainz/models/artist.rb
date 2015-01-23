8b5b25f5d877756ea02a05641a45a14f6662d2dc
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-01-23 20:36:05 +0100

    Add option to add offset Artist#release_groups search. Also include artist credits in Artist#release_groups search.

--- lib/musicbrainz/models/artist.rb
+++ lib/musicbrainz/models/artist.rb
@@ -7,18 +7,21 @@ module MusicBrainz
     # user_tags user_ratings
     INCLUDES = %w(release_groups releases works recordings aliases tags ratings discids media puids isrcs artist_credits various_artists annotation url_rels artist_rels label_rels recording_rels release_rels release_group_rels work_rels)
     
-    def release_groups
+    def release_groups(options = {})
       return @release_groups if @release_groups || @id.nil? || do_not_search
       
-      @release_groups ||= client.search(
-        'MusicBrainz::ReleaseGroup', { artist: id, inc: [:url_rels], limit: 500 }, sort: :first_release_date
+      params = { artist: id, inc: [:url_rels, :artist_credits], limit: 500 }
+      params[:offset] = options[:offset] if options.has_key? :offset
+      
+      client.search(
+        'MusicBrainz::ReleaseGroup', params, sort: :first_release_date
       )
     end
     
     class << self
       def search(name)
         name = CGI.escape(name).gsub(/\!/, '\!')
-        client.search(to_s, "artist:#{name}", create_models: false)
+        client.search(to_s, "artist:\"#{name}\"", create_models: false)
       end
 
       def discography(mbid)
