07e10a069997aeccb0b0681538ff5b644b55113d
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-02-19 11:29:36 +0100

    Implements extra_query option for MusicBrainz::Artist#release_groups.

--- lib/musicbrainz/models/artist.rb
+++ lib/musicbrainz/models/artist.rb
@@ -10,7 +10,8 @@ module MusicBrainz
     def release_groups(options = {})
       return @release_groups if @release_groups || @id.nil? || do_not_search
       
-      params = { artist: id, inc: [:url_rels, :artist_credits], limit: 100 }
+      extra_query = options[:extra_query] ? options[:extra_query] : ''
+      params = { query: "arid:#{id} #{extra_query}", inc: [:url_rels, :artist_credits], limit: 100 }
       params[:offset] = options[:offset] if options.has_key? :offset
       
       client.search(
