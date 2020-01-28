fb9f55c836f4b26d31189e41f2d69a9c4e03c6a7
raderj89 <raderj89@gmail.com>
2020-01-27 20:05:48 -0800

    Add area; fix desc

--- lib/musicbrainz/bindings/artist_search.rb
+++ lib/musicbrainz/bindings/artist_search.rb
@@ -9,9 +9,10 @@ module MusicBrainz
             mbid: (xml.attribute('id').value rescue nil), # Old shit
             name: (xml.xpath('./name').text.gsub(/[`’]/, "'") rescue nil),
             sort_name: (xml.xpath('./sort-name').gsub(/[`’]/, "'") rescue nil),
+            area: (xml.xpath('./area').children.first.children.text rescue nil),
             type: (xml.attribute('type').value rescue nil),
             score: (xml.attribute('score').value.to_i rescue nil),
-            desc: (xml.xpath('./disambiguation').value rescue nil),
+            desc: (xml.xpath('./disambiguation').children.first.text rescue nil),
             aliases: (xml.xpath('./alias-list/alias').map{ |xml| xml.text } rescue [])
           } rescue nil
         end.delete_if{ |item| item.nil? }
