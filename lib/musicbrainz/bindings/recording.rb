50c40f46f107eb1841ce4b5c7941549388fb1aa8
Gregory Eremin <g@erem.in>
2024-03-20 19:57:40 +0100

    Fix indentation

--- lib/musicbrainz/bindings/recording.rb
+++ lib/musicbrainz/bindings/recording.rb
@@ -3,15 +3,15 @@ module MusicBrainz
   module Bindings
     module Recording
       def parse(xml)
-				xml = xml.xpath('./recording') unless xml.xpath('./recording').empty?
+        xml = xml.xpath('./recording') unless xml.xpath('./recording').empty?
         {
-					id: (xml.attribute('id').value rescue nil),
-					mbid: (xml.attribute('id').value rescue nil), # Old shit
-					title: (xml.xpath('./title').text.gsub(/[`’]/, "'") rescue nil),
-					artist: (xml.xpath('./artist-credit/name-credit/artist/name').text rescue nil),
-					releases: (xml.xpath('./release-list/release/title').map{ |xml| xml.text } rescue []),
-					score: (xml.attribute('score').value.to_i rescue nil)
-				}
+          id: (xml.attribute('id').value rescue nil),
+          mbid: (xml.attribute('id').value rescue nil), # Old shit
+          title: (xml.xpath('./title').text.gsub(/[`’]/, "'") rescue nil),
+          artist: (xml.xpath('./artist-credit/name-credit/artist/name').text rescue nil),
+          releases: (xml.xpath('./release-list/release/title').map{ |xml| xml.text } rescue []),
+          score: (xml.attribute('score').value.to_i rescue nil)
+        }
       end
 
       extend self
