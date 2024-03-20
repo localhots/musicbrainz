c7b30c5c7da6feed0ed973bd1c88cdb66f7e1db4
Gregory Eremin <g@erem.in>
2024-03-20 21:52:17 +0100

    Update base model spec

--- spec/models/base_model_spec.rb
+++ spec/models/base_model_spec.rb
@@ -3,38 +3,68 @@ require "spec_helper"
 describe MusicBrainz::BaseModel do
   describe '#validate_type' do
     describe 'Date' do
+      let(:xml) {
+        Nokogiri::XML.parse(response)
+      }
+      let(:bindings) {
+        MusicBrainz::Bindings::ReleaseGroup.parse(xml)
+      }
+      let(:release_group) {
+        MusicBrainz::ReleaseGroup.new(bindings)
+      }
+
       context 'nil value' do
+        let(:response) {
+          <<-XML
+            <release-group>
+              <first-release-date></first-release-date>
+            </release-group>
+          XML
+        }
+
         it 'returns 2030-12-31' do
-          response = '<release-group><first-release-date></first-release-date></release-group>'
-          xml = Nokogiri::XML.parse(response)
-          release_group = MusicBrainz::ReleaseGroup.new MusicBrainz::Bindings::ReleaseGroup.parse(xml)
           expect(release_group.first_release_date).to eq Date.new(2030, 12, 31)
         end
       end
 
       context 'year only' do
+        let(:response) {
+          <<-XML
+            <release-group>
+              <first-release-date>1995</first-release-date>
+            </release-group>
+          XML
+        }
+
         it 'returns 1995-12-31' do
-          response = '<release-group><first-release-date>1995</first-release-date></release-group>'
-          xml = Nokogiri::XML.parse(response)
-          release_group = MusicBrainz::ReleaseGroup.new MusicBrainz::Bindings::ReleaseGroup.parse(xml)
           expect(release_group.first_release_date).to eq Date.new(1995, 12, 31)
         end
       end
 
       context 'year and month only' do
+        let(:response) {
+          <<-XML
+            <release-group>
+              <first-release-date>1995-04</first-release-date>
+            </release-group>
+          XML
+        }
+
         it 'returns 1995-04-30' do
-          response = '<release-group><first-release-date>1995-04</first-release-date></release-group>'
-          xml = Nokogiri::XML.parse(response)
-          release_group = MusicBrainz::ReleaseGroup.new MusicBrainz::Bindings::ReleaseGroup.parse(xml)
           expect(release_group.first_release_date).to eq Date.new(1995, 4, 30)
         end
       end
 
       context 'year, month and day' do
+        let(:response) {
+          <<-XML
+            <release-group>
+              <first-release-date>1995-04-30</first-release-date>
+            </release-group>
+          XML
+        }
+
         it 'returns 1995-04-30' do
-          response = '<release-group><first-release-date>1995-04-30</first-release-date></release-group>'
-          xml = Nokogiri::XML.parse(response)
-          release_group = MusicBrainz::ReleaseGroup.new MusicBrainz::Bindings::ReleaseGroup.parse(xml)
           expect(release_group.first_release_date).to eq Date.new(1995, 4, 30)
         end
       end
